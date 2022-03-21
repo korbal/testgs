4e3ac08d5a8ef06e7c202666142ee5374ba59bff
Bálint <balint.j.korosi@gmail.com>
2022-03-21 15:40:16 +0100

    Update my.js

--- my.js
+++ my.js
@@ -8,4 +8,29 @@ function testGS() {
     })
 }
 
+
+
+function addGS() {
+  const url = "https://script.google.com/macros/s/AKfycbyF-skKhvL961u4KROfBhW6aa8jUzfoiX9govOoJQV5HVGfFeg/exec"
+
+  fetch(url,
+
+    {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'no-cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({ name: "Olivia" }) // body data type must match "Content-Type" header
+    }
+
+  )
+
+}
 document.getElementById("btn").addEventListener("click", testGS)
+document.getElementById("btn2").addEventListener("click", addGS)
